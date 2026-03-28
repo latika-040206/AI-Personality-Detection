2181d080a6c45cb2a213a9368f6e069f52e49311
Latika Shekhawat <latika.25bai10928@vitbhopal.ac.in>
2026-03-28 18:05:17 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,12 @@
+import joblib
 
+model = joblib.load("../models/personality_model.pkl")
+vectorizer = joblib.load("../models/vectorizer.pkl")
+
+text = input("Enter a sentence: ")
+
+text_vector = vectorizer.transform([text])
+
+prediction = model.predict(text_vector)
+
+print("Predicted Personality:", prediction[0])
